1cfc21c54df304563467c145f18ea863b9b8be65
sov thade tage em ereb <98284211+irulanCorrino@users.noreply.github.com>
2022-10-31 03:24:37 +0300

    muing [karh.: numeric death]
    cannot count to three so uploading src in FOUR turns
    ~`98+14+98+x == some TWO HUNDREDS`~

---
+++ src/UIHandle.cpp
@@ -0,0 +1,51 @@
+/**********************************************************************
+
+Audacity: A Digital Audio Editor
+
+UIHandle.cpp
+
+Paul Licameli
+
+**********************************************************************/
+
+#include "Audacity.h"
+#include "UIHandle.h"
+
+#include "RefreshCode.h"
+
+UIHandle::~UIHandle()
+{
+}
+
+void UIHandle::Enter(bool, AudacityProject *)
+{
+}
+
+bool UIHandle::HasRotation() const
+{
+   return false;
+}
+
+bool UIHandle::Rotate(bool)
+{
+   return false;
+}
+
+bool UIHandle::HasEscape() const
+{
+   return false;
+}
+
+bool UIHandle::Escape(AudacityProject *)
+{
+   return false;
+}
+
+bool UIHandle::StopsOnKeystroke()
+{
+   return false;
+}
+
+void UIHandle::OnProjectChange(AudacityProject *)
+{
+}
